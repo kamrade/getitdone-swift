9cdd128def785bcc74cab7907be7e7e8155621ce
Denis Mikhailov <denis.michailov@gmail.com>
2019-06-11 14:31:12 +1000

    Initial Commit

---
+++ Getitdone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Getitdone
+//
+//  Created by Denis Michailov on 11/06/2019.
+//  Copyright © 2019 Denis Michailov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
