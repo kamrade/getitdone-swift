67d6dce9a2914f4e36329254fce48e4180edd2c0
Denis Mikhailov <denis.michailov@gmail.com>
2019-06-11 18:09:20 +1000

    Save

--- Getitdone/AppDelegate.swift
+++ Getitdone/AppDelegate.swift
@@ -17,6 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let wc = WelcomeController()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = wc
+
         return true
     }
 
---
+++ Getitdone/WelcomeController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Getitdone
+//
+//  Created by Denis Michailov on 11/06/2019.
+//  Copyright © 2019 Denis Michailov. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
